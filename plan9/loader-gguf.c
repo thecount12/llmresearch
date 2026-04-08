e97182d8c0f8aba57bd3dbb7f84ce4ee5d2acf3e
William Gunnells <gunnells@gmail.com>
2026-04-08 01:07:44 -0500

    quantizied tensor

--- plan9/loader-gguf.c
+++ plan9/loader-gguf.c
@@ -14,11 +14,11 @@ enum {
 	GGUFArray,
 	GGUFUint64,
 	GGUFInt64,
-	GGUFFloat64,
-	GGUFTypeCount
+	GGUFFloat64
 };
 
 typedef struct GGUFInfo GGUFInfo;
+typedef struct GGUFMap GGUFMap;
 struct GGUFInfo {
 	ulong version;
 	uvlong tensor_count;
@@ -32,6 +32,24 @@ struct GGUFInfo {
 	uvlong n_kv_heads;
 	uvlong seq_len;
 	ulong alignment;
+};
+
+struct GGUFMap {
+	int token_embd;
+	int output_norm;
+	int output;
+	int attn_norm;
+	int attn_q;
+	int attn_k;
+	int attn_v;
+	int attn_out;
+	int ffn_norm;
+	int ffn_gate;
+	int ffn_down;
+	int ffn_up;
+	int unknown;
+	int quantized;
+	ulong first_type;
 };
 
 static int
@@ -125,23 +143,6 @@ readf64(int fd, double *out)
 	if(readu64(fd, &v.u) < 0)
 		return -1;
 	*out = v.d;
-	return 0;
-}
-
-static int
-skipbytes(int fd, uvlong n)
-{
-	uchar buf[256];
-	int chunk;
-
-	while(n > 0){
-		chunk = sizeof(buf);
-		if(n < (uvlong)chunk)
-			chunk = (int)n;
-		if(readfull9(fd, buf, chunk) < 0)
-			return -1;
-		n -= chunk;
-	}
 	return 0;
 }
 
@@ -363,28 +364,142 @@ parse_metadata(int fd, GGUFInfo *gi)
 }
 
 static int
-skip_tensor_infos(int fd, uvlong n)
+parseblklayer(char *name, char *suffix)
+{
+	char *p, *q;
+	int n, want;
+
+	p = name;
+	if(strncmp(p, "blk.", 4) != 0)
+		return -1;
+	p += 4;
+	n = 0;
+	while(*p >= '0' && *p <= '9'){
+		n = n * 10 + (*p - '0');
+		p++;
+	}
+	if(*p != '.')
+		return -1;
+	q = p + 1;
+	want = strlen(suffix);
+	if(strcmp(q, suffix) == 0)
+		return n;
+	return -1;
+}
+
+static int
+istensorf32(ulong ggml_type)
+{
+	return ggml_type == 0;
+}
+
+static void
+updatemap(GGUFMap *gm, char *name, ulong ggml_type)
+{
+	if(!istensorf32(ggml_type)){
+		gm->quantized++;
+		if(gm->first_type == 0)
+			gm->first_type = ggml_type;
+	}
+
+	if(strcmp(name, "token_embd.weight") == 0){
+		gm->token_embd++;
+		return;
+	}
+	if(strcmp(name, "output_norm.weight") == 0){
+		gm->output_norm++;
+		return;
+	}
+	if(strcmp(name, "output.weight") == 0){
+		gm->output++;
+		return;
+	}
+	if(parseblklayer(name, "attn_norm.weight") >= 0){
+		gm->attn_norm++;
+		return;
+	}
+	if(parseblklayer(name, "attn_q.weight") >= 0){
+		gm->attn_q++;
+		return;
+	}
+	if(parseblklayer(name, "attn_k.weight") >= 0){
+		gm->attn_k++;
+		return;
+	}
+	if(parseblklayer(name, "attn_v.weight") >= 0){
+		gm->attn_v++;
+		return;
+	}
+	if(parseblklayer(name, "attn_output.weight") >= 0){
+		gm->attn_out++;
+		return;
+	}
+	if(parseblklayer(name, "ffn_norm.weight") >= 0){
+		gm->ffn_norm++;
+		return;
+	}
+	if(parseblklayer(name, "ffn_gate.weight") >= 0){
+		gm->ffn_gate++;
+		return;
+	}
+	if(parseblklayer(name, "ffn_down.weight") >= 0){
+		gm->ffn_down++;
+		return;
+	}
+	if(parseblklayer(name, "ffn_up.weight") >= 0){
+		gm->ffn_up++;
+		return;
+	}
+	gm->unknown++;
+}
+
+static int
+parse_tensor_infos(int fd, uvlong n, GGUFMap *gm)
 {
 	uvlong i, j, dimv, off;
 	ulong ndims, ggml_type;
 	char *name;
 
+	memset(gm, 0, sizeof(*gm));
 	for(i = 0; i < n; i++){
 		if(readstr(fd, &name) < 0)
 			return -1;
-		free(name);
-		if(readu32(fd, &ndims) < 0)
+		if(readu32(fd, &ndims) < 0){
+			free(name);
 			return -1;
-		for(j = 0; j < ndims; j++){
-			if(readu64(fd, &dimv) < 0)
-				return -1;
 		}
-		if(readu32(fd, &ggml_type) < 0)
+		for(j = 0; j < ndims; j++){
+			if(readu64(fd, &dimv) < 0){
+				free(name);
+				return -1;
+			}
+		}
+		if(readu32(fd, &ggml_type) < 0){
+			free(name);
 			return -1;
-		if(readu64(fd, &off) < 0)
+		}
+		if(readu64(fd, &off) < 0){
+			free(name);
 			return -1;
+		}
+		updatemap(gm, name, ggml_type);
+		free(name);
 	}
 	return 0;
+}
+
+static int
+hasrequired(GGUFInfo *gi, GGUFMap *gm)
+{
+	if(gm->token_embd < 1 || gm->output_norm < 1 || gm->output < 1)
+		return 0;
+	if(gm->attn_norm < gi->n_layers || gm->attn_q < gi->n_layers ||
+	   gm->attn_k < gi->n_layers || gm->attn_v < gi->n_layers ||
+	   gm->attn_out < gi->n_layers || gm->ffn_norm < gi->n_layers ||
+	   gm->ffn_gate < gi->n_layers || gm->ffn_down < gi->n_layers ||
+	   gm->ffn_up < gi->n_layers)
+		return 0;
+	return 1;
 }
 
 int
@@ -394,6 +509,7 @@ load_model_gguf(Model *m, char *path, char *err, int nerr)
 	uchar magic[4];
 	ulong version;
 	GGUFInfo gi;
+	GGUFMap gm;
 
 	USED(m);
 
@@ -427,7 +543,7 @@ load_model_gguf(Model *m, char *path, char *err, int nerr)
 		close(fd);
 		return -1;
 	}
-	if(skip_tensor_infos(fd, gi.tensor_count) < 0){
+	if(parse_tensor_infos(fd, gi.tensor_count, &gm) < 0){
 		snprint(err, nerr, "failed parsing gguf tensor table");
 		close(fd);
 		return -1;
@@ -441,8 +557,24 @@ load_model_gguf(Model *m, char *path, char *err, int nerr)
 	if(gi.n_kv_heads == 0 && gi.n_heads > 0)
 		gi.n_kv_heads = gi.n_heads;
 
+	if(!hasrequired(&gi, &gm)){
+		snprint(err, nerr,
+			"gguf parsed but tensor mapping incomplete: arch=%s layers=%llud token_embd=%d output_norm=%d output=%d attn_norm=%d attn_q=%d attn_k=%d attn_v=%d attn_out=%d ffn_norm=%d ffn_gate=%d ffn_down=%d ffn_up=%d unknown=%d",
+			gi.architecture, gi.n_layers, gm.token_embd, gm.output_norm, gm.output,
+			gm.attn_norm, gm.attn_q, gm.attn_k, gm.attn_v, gm.attn_out,
+			gm.ffn_norm, gm.ffn_gate, gm.ffn_down, gm.ffn_up, gm.unknown);
+		return -1;
+	}
+	if(gm.quantized > 0){
+		snprint(err, nerr,
+			"gguf mapped: arch=%s version=%lud tensors=%llud layers=%llud dim=%llud heads=%llud kv_heads=%llud vocab=%llud ctx=%llud; quantized tensor loading not implemented (first ggml_type=%lud, mapped layers ok)",
+			gi.architecture, gi.version, gi.tensor_count, gi.n_layers, gi.dim,
+			gi.n_heads, gi.n_kv_heads, gi.vocab_size, gi.seq_len, gm.first_type);
+		return -1;
+	}
+
 	snprint(err, nerr,
-		"gguf parsed: arch=%s version=%lud tensors=%llud kv=%llud dim=%llud layers=%llud heads=%llud kv_heads=%llud vocab=%llud ctx=%llud; tensor loading not implemented",
+		"gguf mapped: arch=%s version=%lud tensors=%llud kv=%llud dim=%llud layers=%llud heads=%llud kv_heads=%llud vocab=%llud ctx=%llud; float32 tensor loading not implemented",
 		gi.architecture, gi.version, gi.tensor_count, gi.kv_count, gi.dim,
 		gi.n_layers, gi.n_heads, gi.n_kv_heads, gi.vocab_size, gi.seq_len);
 	return -1;
